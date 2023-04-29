307873cc6a50e364e048408dfdc8bbfaf76f9acb
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-29 18:18:24 +0600

    install to bootstrap

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Register = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        console.log(email,password);
+    }
     return (
         <div>
-            <h2>this is register</h2>
+            <h2>Please Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="email" name="email" id="email" placeholder='Your Email' />
+                <br /> 
+                <input type="password" name="password" id="password" placeholder='Your Password' />
+                <br /> 
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
