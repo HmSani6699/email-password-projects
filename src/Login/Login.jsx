333b491f4ce8f57aa2cd6dc9730cc533e9c79036
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-30 11:19:39 +0600

    ad to reset password

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,19 +1,18 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import app from '../Firebase/Firebase.config';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const [error,setError]=useState('');
-    const [success,setSuccess]=useState('');
-    const auth = getAuth(app)
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const auth = getAuth(app);
+    const emailReset = useRef();
 
     const handleSubmit = event => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password);
-
 
         signInWithEmailAndPassword(auth, email, password)
             .then(result => {
@@ -27,13 +26,24 @@ const Login = () => {
                 setError(error.message)
             })
     }
+
+    const handleResetBtn = ()=> {
+        const email = emailReset.current.value;
+        sendPasswordResetEmail(auth,email)
+        .then(()=>{
+            alert('Please check your email')
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
     return (
         <div>
             <h2>Please login</h2>
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label /*for="exampleInputEmail1"*/ className="form-label">Email address</label>
-                    <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" ref={emailReset} name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
 
                 </div>
                 <div className="mb-3">
@@ -45,6 +55,7 @@ const Login = () => {
                     <label className="form-check-label" /*for="exampleCheck1"*/ >Check me out</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <p><small>Reset password <button onClick={handleResetBtn} className='btn btn-link'>Reset</button></small></p>
                 <p>New to this website ? <Link to='/register'>Register</Link> </p>
                 <p className='text-danger'>{error}</p>
                 <p className='text-success'>{success}</p>
