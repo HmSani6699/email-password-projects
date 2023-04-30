e9ca4460122618e48aaa117da4365e3eb75ba7e3
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-30 22:40:30 +0600

    recap to your all  file in projects

--- src/__stap.js
+++ src/__stap.js
@@ -9,6 +9,7 @@
 * 8. (login === sign in) signInWithEmailAndPassword
 * 9. verify the emaill for [visit] (go to docs > build > authentication > web > Manage user >      sent a user a verification email)
 * 10. import senVerificationEmail and sendEmailVerification(currentUser)
-*
-*
-**/ 
+* 11. reast this password [visit] (go to docs > build > authentication > web > Manage user >      sendPasswordResetEmail)
+* 12. update profile [visit] (go to docs > build > authentication > web > Manage user > Update User's Profile)
+* 13. validation to [visit] regular expression
+*/ 
