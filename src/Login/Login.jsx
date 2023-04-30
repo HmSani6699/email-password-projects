40edfd3b693637f26d1e1614cc026248b161e2cf
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-30 07:49:31 +0600

    ad a new form

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Login = () => {
+    const handleSubmit =event=>{
+        event.preventDefault();
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        console.log(email,password);
+    }
     return (
         <div>
-            <h2>This login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label /*for="exampleInputEmail1"*/ className="form-label">Email address</label>
+                    <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        
+                </div>
+                <div className="mb-3">
+                    <label /*for="exampleInputPassword1"*/ className="form-label">Password</label>
+                    <input type="password" name='password' className="form-control" id="exampleInputPassword1" />
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                        <label className="form-check-label" /*for="exampleCheck1"*/ >Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 };
