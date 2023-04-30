0a6ebc79935f4c819082c623d7e29c396561eec4
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-30 23:33:10 +0600

    added to show password button

--- src/Register__1/Register__1.jsx
+++ src/Register__1/Register__1.jsx
@@ -4,8 +4,9 @@ import app from '../Firebase/Firebase.config';
 import { Link } from 'react-router-dom';
 
 const Register__1 = () => {
-    const [error, setError] = useState('')
-    const [success, setSuccess] = useState('')
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [passwordType, setPasswordType] = useState(false);
     const auth = getAuth(app);
 
 
@@ -50,18 +51,30 @@ const Register__1 = () => {
     // update profilr
     const handleUpdateProfile = (user, name) => {
         updateProfile(user, {
-            displayName:name,
+            displayName: name,
         })
     }
+
+    // handle show password
+    const handleShowPassword = () => {
+        setPasswordType(!passwordType);
+        return
+    }
+
+
     return (
         <div>
+            {/* {passwordType ? setPsType('text') : setPsType('password')} */}
+        
             <h2 className='mt-3 mb-3 text-center text-primary'>Please register !!</h2>
             <form onSubmit={handleFormSubmit}>
                 <input className='mb-3 w-100 rounded-4 p-2 ps-2' type="text" name="name" id="text" placeholder='Your Name' required />
                 <br />
                 <input className='mb-3 w-100 rounded-4 p-2 ps-2' type="email" name="email" id="email" placeholder='Your Email' required />
                 <br />
-                <input className='mb-3 w-100 rounded-4 p-2 ps-2' type='password' name="password" id="password" placeholder='Your Password' required />
+                <input className='mb-3 w-100 rounded-4 p-2 ps-2' type={passwordType?'text':'password'} name="password" id="password" placeholder='Your Password' required />
+                <br />
+                <button onClick={handleShowPassword} className='btn btn-success mt-3 mb-3'>Show Password</button>
                 <br />
                 <input className='btn btn-primary' type="submit" value="Register" />
                 <p>Please login ?<Link to='/login__1'>Login_-_1</Link></p>
