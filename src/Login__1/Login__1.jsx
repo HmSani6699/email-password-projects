a46531518ad381670928896bf421e2df87dd0cad
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-04-30 22:05:43 +0600

    ad email verification

--- src/Login__1/Login__1.jsx
+++ src/Login__1/Login__1.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import app from '../Firebase/Firebase.config';
 
 const Login__1 = () => {
+const [error,setError] =useState('');
+const [success,setSuccess] =useState('')
+    const auth =getAuth(app);
+
+    const handleFormSubmit=event=>{
+        const form =event.target;
+        event.preventDefault();
+        const email=form.email.value;
+        const password =form.password.value;
+        console.log(email,password);
+
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result=>{
+            const loggedUser=result.user;
+            console.log(loggedUser);
+            setSuccess('Success full logitn');
+            setError('')
+        })
+        .catch(error=>{
+            console.log(error);
+            setError(error.message)
+        })
+    }
+
     return (
         <div>
             <h2 className='mt-3 mb-3 text-center text-primary'>Please login !!</h2>
-            <form >
+            <form onSubmit={handleFormSubmit}>
                 <input className='mb-3 w-100 rounded-4 p-2 ps-2' type="email" name="email" id="email" placeholder='Your Email' required />
                 <br />
                 <input className='mb-3 w-100 rounded-4 p-2 ps-2' type='password' name="password" id="password" placeholder='Your Password' required />
@@ -13,6 +39,8 @@ const Login__1 = () => {
                 <input className='btn btn-primary' type="submit" value="Login" />
                 <p>Please register now ? <Link to='/register__1'>Register_-_1</Link></p>
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
